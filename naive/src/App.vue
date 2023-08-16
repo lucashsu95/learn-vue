846118127ed4a01856b4913b561628631c0dd569
lucashsu95 <lucashsu95@gmail.com>
2023-08-16 23:06:09 +0800

    0816_23:06

---
+++ naive/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { NCard } from 'naive-ui';
+import TheForm from './components/TheForm.vue'
+import TheTabs from './components/TheTabs.vue';
+import TheSignAndLogin from './components/TheSignAndLogin.vue';
+</script>
+<template>
+  <div class="container">
+
+    <!-- component 1 -->
+    <n-card>
+      <TheForm></TheForm>
+    </n-card>
+    <hr>
+    <!-- component 2 -->
+    <TheTabs></TheTabs>
+    <hr>
+    <!-- component 3 -->
+    <TheSignAndLogin></TheSignAndLogin>
+
+
+  </div>
+</template>
+
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 80%;
+}
+</style>
---
+++ naive/src/components/TheSignAndLogin.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { NButton, NInput, NForm, NCard, NTabs, NTabPane, NFormItemRow } from 'naive-ui';
+
+</script>
+<template>
+    <n-card>
+        <n-tabs class="card-tabs" default-value="signin" size="large" animated pane-wrapper-style="margin: 0 -4px"
+            pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;">
+            <n-tab-pane name="signin" tab="登录">
+                <n-form>
+                    <n-form-item-row label="用户名">
+                        <n-input />
+                    </n-form-item-row>
+                    <n-form-item-row label="密码">
+                        <n-input />
+                    </n-form-item-row>
+                </n-form>
+
+                <n-button type="primary" block secondary strong>
+                    登录
+                </n-button>
+
+            </n-tab-pane>
+            <n-tab-pane name="signup" tab="注册">
+                <n-form>
+                    <n-form-item-row label="用户名">
+                        <n-input />
+                    </n-form-item-row>
+                    <n-form-item-row label="密码">
+                        <n-input />
+                    </n-form-item-row>
+                    <n-form-item-row label="重复密码">
+                        <n-input />
+                    </n-form-item-row>
+                </n-form>
+
+                <n-button type="primary" block secondary strong>
+                    注册
+                </n-button>
+
+            </n-tab-pane>
+        </n-tabs>
+    </n-card>
+</template>
+<style scoped>
+.card-tabs .n-tabs-nav--bar-type {
+    padding-left: 4px;
+}
+</style>
